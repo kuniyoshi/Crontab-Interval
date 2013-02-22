8896bb68b6dc80fd5ea0eddfd8fafce38c9a7629
Kuniyoshi Kouji <kuniyoshi@seesaa.co.jp>
2013-02-22 11:29:09 +0900

    initial commit

---
+++ lib/Cron/Interval.pm
@@ -0,0 +1,32 @@
+package Cron::Interval;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Cron::Interval -
+
+=head1 SYNOPSIS
+
+  use Cron::Interval;
+
+=head1 DESCRIPTION
+
+Cron::Interval is
+
+=head1 AUTHOR
+
+Kuniyoshi Kouji E<lt>kuniyoshi@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
